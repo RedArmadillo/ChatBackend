ab7cc027fbffde0ad561f8062da060d6364dee58
Alyssa <pilocchii@localhost.localdomain>
2018-05-02 20:12:21 -0700

    adds a skeleton for routes

---
+++ routes/chat.js
@@ -0,0 +1,8 @@
+/*
+
+DROP TABLE IF EXISTS Chats;
+CREATE TABLE Chats (ChatID SERIAL PRIMARY KEY,
+                    Name VARCHAR(255)
+);
+
+*/
---
+++ routes/user.js
@@ -0,0 +1,24 @@
+
+
+/* 
+DROP TABLE IF EXISTS Members;
+CREATE TABLE Members (MemberID SERIAL PRIMARY KEY,
+                      FirstName VARCHAR(255) NOT NULL,
+		              LastName VARCHAR(255) NOT NULL,
+                      Username VARCHAR(255) NOT NULL UNIQUE,
+                      Email VARCHAR(255) NOT NULL UNIQUE,
+                      Password VARCHAR(255) NOT NULL,
+                      SALT VARCHAR(255),
+                      Verification INT DEFAULT 0
+
+DROP TABLE IF EXISTS Chats;
+CREATE TABLE Chats (ChatID SERIAL PRIMARY KEY,
+                    Name VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS ChatMembers;
+CREATE TABLE ChatMembers (ChatID INT NOT NULL,
+                          MemberID INT NOT NULL,
+                          FOREIGN KEY(MemberID) REFERENCES Members(MemberID),
+                          FOREIGN KEY(ChatID) REFERENCES Chats(ChatID)
+);
