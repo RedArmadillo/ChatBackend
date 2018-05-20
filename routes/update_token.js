065d295eef178ab21a91e51f36f0ab1a678836ea
Trinh <trinhpn@uw.edu>
2018-05-20 00:45:17 -0700

    fix wrong param in query of update token

--- routes/update_token.js
+++ routes/update_token.js
@@ -10,7 +10,7 @@ router.use(bodyParser.json());
 router.post('/', (req, res) => {
     let memberid = req.body['memberid'];
     let token = req.body['token'];
-    db.none("update members set firebase_token = $1 where username = $2", [token, memberid])
+    db.none("update members set firebase_token = $1 where memberid = $2", [token, memberid])
     .then(()=>{
         res.send({
             success: true,
